12b2a936273f5ea830ea4c3f07496bb49270a575
bitshen <161296688+bitshen@users.noreply.github.com>
2024-03-01 13:59:52 +0800

    Update python_org_search.py

--- python_org_search.py
+++ python_org_search.py
@@ -1,21 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
 
-driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+def test_eight_components():
+    driver = webdriver.Chrome()
 
-title = driver.title
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
-driver.implicitly_wait(0.5)
+    title = driver.title
+    assert title == "Web form"
 
-text_box = driver.find_element(by=By.NAME, value="my-text")
-submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+    driver.implicitly_wait(0.5)
 
-text_box.send_keys("Selenium")
-submit_button.click()
+    text_box = driver.find_element(by=By.NAME, value="my-text")
+    submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
 
-message = driver.find_element(by=By.ID, value="message")
-text = message.text
+    text_box.send_keys("Selenium")
+    submit_button.click()
 
-driver.quit()
+    message = driver.find_element(by=By.ID, value="message")
+    value = message.text
+    assert value == "Received!"
+
+    driver.quit()
