1676dee7857ccf0945d5f740d597957d191df769
bitshen <161296688+bitshen@users.noreply.github.com>
2024-03-01 11:16:58 +0800

    Update python_org_search.py

--- python_org_search.py
+++ python_org_search.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys  # 导入缺失的Keys
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service  # 导入Service类
 from webdriver_manager.firefox import GeckoDriverManager
 
 options = Options()
 options.headless = True
-driver = webdriver.Firefox(options=options, executable_path=GeckoDriverManager().install())
+service = Service(executable_path=GeckoDriverManager().install())  # 使用Service对象
+driver = webdriver.Firefox(options=options, service=service)
 
 driver.get("http://www.python.org")
 assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
+elem = driver.find_element("name", "q")  # 更新了find_element的调用方式
 elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
