ba57dba34a6a0a255e68e678d911c4e63f47cd78
bitshen <161296688+bitshen@users.noreply.github.com>
2024-03-01 10:49:03 +0800

    Update python_org_search.py

--- python_org_search.py
+++ python_org_search.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
