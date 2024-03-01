fc068bc8f0a0be44dc5a150376d33940c860b68b
bitshen <161296688+bitshen@users.noreply.github.com>
2024-03-01 11:34:40 +0800

    Update python_org_search.py

--- python_org_search.py
+++ python_org_search.py
@@ -1,19 +1,27 @@
+# test_python_org_search.py
+import unittest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys  # 导入缺失的Keys
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service  # 导入Service类
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.keys import Keys
 
-options = Options()
-options.headless = True
-service = Service(executable_path=GeckoDriverManager().install())  # 使用Service对象
-driver = webdriver.Firefox(options=options, service=service)
+class PythonOrgSearch(unittest.TestCase):
 
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element("name", "q")  # 更新了find_element的调用方式
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+    def setUp(self):
+        # 初始化WebDriver
+        self.driver = webdriver.Firefox()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element("name", "q")
+        elem.clear()
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+    def tearDown(self):
+        # 关闭浏览器窗口
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
